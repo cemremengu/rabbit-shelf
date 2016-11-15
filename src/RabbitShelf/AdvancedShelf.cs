d14aef6a5689ef9a62508edf19e5ff4b1d9545ef
cemre.mengu <cemre.mengu@ttgint.com>
2016-11-15 14:07:49 +0300

    Added regular bus support in addition to advanced bus

---
+++ src/RabbitShelf/AdvancedShelf.cs
@@ -0,0 +1,29 @@
+﻿namespace RabbitShelf
+{
+    using System;
+    using EasyNetQ;
+
+    public abstract class AdvancedShelf : IRabbit
+    {
+        private AdvancedShelf()
+        {
+        }
+
+        protected AdvancedShelf(IAdvancedBus bus)
+        {
+            Bus = bus;
+        }
+
+        public IAdvancedBus Bus { get; }
+
+        public virtual void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
